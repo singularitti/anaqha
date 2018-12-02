d3503769d6cf5c58b98c52df6aa455788c5a8757
Qi Zhang <singularitti@outlook.com>
2018-12-01 21:53:56 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hybridqha',
+    version='0.0.1',
+    packages=['hybridqha'],
+    url='https://github.com/singularitti/hybridqha',
+    license='GNU General Public License 3',
+    author='Qi Zhang',
+    author_email='singularitti@outlook.com',
+    description='A hybrid version of qha'
+)
